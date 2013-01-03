864d80943ae03821650bd657d8a6f5e0857950d9
Daniel D. Shaw <github@dshaw.com>
2013-01-03 14:58:55 -0800

    Add support for short options. -S --sources List sources -s --source View articles for source -a --article Select article number -o --output Output: default, json, url -O --open Open article in browser

--- bin/routers-news.js
+++ bin/routers-news.js
@@ -1,12 +1,41 @@
 #!/usr/bin/env node
 
-var  argv = require('optimist').argv,
+var exec = require('child_process').exec,
+  optimist = require('optimist'),
   Loader = require('../lib').Loader,
   Source = require('../lib').Source,
   Grabber = require('../lib').Grabber;
 
-if (argv.sources) { // Display al lthe news sources Routers currently parses.
+var argv = optimist
+  .options('S', {
+    alias: 'sources',
+    describe: 'List sources'
+  })
+  .options('s', {
+    alias: 'source',
+    describe: 'List articles from a source'
+  })
+  .options('a', {
+    alias: 'article',
+    describe: 'Select an article number'
+  })
+  .options('o', {
+    alias: 'output',
+    describe: 'Output format: default, json, url'
+  })
+  .options('O', {
+    alias: 'open',
+    describe: 'open the article in your browser'
+  })
+  .argv;
 
+var actions = {
+  sources: sources,
+  articles: listArticles,
+  article: showArticle
+};
+
+function sources () {
   var loader = new Loader(),
     categories = [{
       name: '\033[1;30mRouters News Sources:\033[m\n',
@@ -16,7 +45,7 @@ if (argv.sources) { // Display al lthe news sources Routers currently parses.
 
   while (categories.length) {
     var category = categories.pop();
-    console.log(category.indent + category.name)
+    console.log(category.indent + category.name);
     Object.keys(category.data).forEach(function(key) {
       if (category.data[key] instanceof Source) {
         console.log(category.indent + '  ' + category.data[key].source + ': ' + category.data[key].description);
@@ -29,10 +58,12 @@ if (argv.sources) { // Display al lthe news sources Routers currently parses.
       }
     });
   }
-} else if (argv.source && !argv.article) { // List the headlines for a source.
+}
+
+function listArticles (source) {
   var grabber = new Grabber();
 
-  grabber.grabHeadlines((argv.source || argv.headlines), function(err, headlines) {
+  grabber.grabHeadlines((source), function(err, headlines) {
     if (err) {
       console.log(err.message);
       return;
@@ -43,11 +74,13 @@ if (argv.sources) { // Display al lthe news sources Routers currently parses.
       console.log('\t\033[32m' + headline.href + '\033[m\n');
     }
   });
-} else if (argv.article) {
-  var grabber = new Grabber(),
-    index = parseInt(argv.article) - 1;
+}
 
-  grabber.grabHeadlines(argv.source, function(err, headlines) {
+function showArticle (source, index) {
+  var grabber = new Grabber();
+  index = parseInt(index) - 1;
+
+  grabber.grabHeadlines(source, function(err, headlines) {
 
     if (err) {
       console.log(err.message);
@@ -59,29 +92,62 @@ if (argv.sources) { // Display al lthe news sources Routers currently parses.
       return;
     }
 
-    grabber.grabArticle(argv.source, headlines[index], function(err, article) {
-      
+    grabber.grabArticle(source, headlines[index], function(err, article) {
+
       if (err) {
         console.log(err.message);
         return;
       }
 
+      article.href = headlines[index].href;
+
+      if (argv.open) {
+        var command = 'open ' + article.href;
+        console.log(command);
+        exec(command);
+        return;
+      }
+
       if (argv.output === 'json') {
-        article.href = headlines[index].href;
         console.log(JSON.stringify(article));
+      } else if (argv.output === 'url') {
+        console.log(article.href);
       } else {
         console.log('\033[1;30m' + article.title + ':\033[m');
         if (article.img) console.log('\n[' + article.img + ']');
         console.log('\n' + article.body + '\n---------' );
-        console.log('\033[32m' + headlines[index].href + '\033[m\n');
+        console.log('\033[32m' + article.href + '\033[m\n');
       }
 
     });
   });
+}
+
+if (argv.debug) {
+  console.log('argv', argv)
+}
+
+// Route actions
+if (argv.sources) {
+
+  // Display all the news sources Routers currently parses.
+  return actions.sources();
+
+} else if (argv.source && !argv.article) {
+
+  // List the headlines for a source.
+  return actions.articles(argv.source);
+
+} else if (argv.article) {
+
+  return actions.article(argv.source, argv.article);
+
 } else {
+
   console.log("Usage:\n\
     \trouters-news --sources\tlist the news sources available.\n\
     \trouters-news --source=[source]\tlist the headlines for a news source.\n\
     \trouters-news --source=[source] --article=[id]\tload an article by integer id.\n\
-  ")
+  ");
+
 }
